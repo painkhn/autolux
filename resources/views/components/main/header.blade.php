4076cb564cfa205d09f3ceff649cf0b05650285c
painkhn <1fotice@gmail.com>
2025-05-27 18:28:01 +0800

    header icons

--- resources/views/components/main/header.blade.php
+++ resources/views/components/main/header.blade.php
@@ -17,11 +17,31 @@
         @endauth
 
         <div class="flex items-center justify-self-end">
-            @if (auth()->user())
-                <button type="button" id="dropdownInformationButton" data-dropdown-toggle="dropdownInformation"  class="font-semibold text-black dark:text-white transition-all hover:underline hover:opacity-80 mr-4">
-                    {{ auth()->user()->name }}
-                </button>
-                <x-user-dropdown />
+            @auth
+                <ul class="flex items-center gap-4">
+                    <li class="flex items-center">
+                        <button type="button" id="dropdownInformationButton" data-dropdown-toggle="dropdownInformation" class="transition-all hover:opacity-80">
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-width="2" d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                            </svg>
+                        </button>
+                        <x-user-dropdown />
+                    </li>
+                    <li>
+                        <a href="#!" class="transition-all hover:opacity-80">
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"/>
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#!" class="transition-all hover:opacity-80">
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"/>
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
             @else
                 <ul class="flex items-center">
                     <li>
@@ -33,7 +53,7 @@
                         <x-login-modal title="login-modal" />
                     </li>
                 </ul>
-            @endif
+            @endauth
             <button
                 onclick="
                     const html = document.documentElement;
