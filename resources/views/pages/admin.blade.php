c4eb0771cc40a23e44b2d415775b62e267a35ab8
painkhn <1fotice@gmail.com>
2025-05-29 02:59:01 +0800

    убейте меня

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -72,18 +72,11 @@
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" class="px-6 py-3">
-                            ID заказа
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Адрес
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Клиент
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Общая сумма заказа
-                        </th>
+                        <th scope="col" class="px-6 py-3">ID заказа</th>
+                        <th scope="col" class="px-6 py-3">Адрес</th>
+                        <th scope="col" class="px-6 py-3">Клиент</th>
+                        <th scope="col" class="px-6 py-3">Общая сумма заказа</th>
+                        <th scope="col" class="px-6 py-3"></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -92,23 +85,56 @@
                             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 # {{ $order->id }}
                             </th>
+                            <td class="px-6 py-4">{{ $order->address }}</td>
                             <td class="px-6 py-4">
-                                {{ $order->address }}
-                            </td>
-                            <td class="px-6 py-4">
-                                <button data-modal-target="order-user-modal" data-modal-toggle="order-user-modal">
+                                <button data-modal-target="order-user-modal-{{ $order->id }}" data-modal-toggle="order-user-modal-{{ $order->id }}">
                                     {{ $order->user->name }}
                                 </button>
                                 <x-order-user-data :user="$order->user" :order="$order" />
                             </td>
                             <td class="px-6 py-4">
-                                $2999
+                                {{ number_format($order->total_price, 0, '', ' ') }} ₽
+                            </td>
+                            <td class="px-6 py-4 relative">
+                                <!-- Кнопка с уникальным ID -->
+                                <button data-modal-target="order-status-modal-{{ $order->id }}" data-modal-toggle="order-status-modal-{{ $order->id }}"
+                                    class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                                    type="button">
+                                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
+                                        <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
+                                    </svg>
+                                </button>
+                                
+                                <div id="order-status-modal-{{ $order->id }}" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                    <div class="relative p-4 w-full max-w-2xl max-h-full">
+                                        <!-- Modal content -->
+                                        <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+                                            <!-- Modal header -->
+                                            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
+                                                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                                    Изменить статус заказа
+                                                </h3>
+                                                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="order-status-modal-{{ $order->id }}">
+                                                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                                    </svg>
+                                                    <span class="sr-only">Close modal</span>
+                                                </button>
+                                            </div>
+                                            <!-- Modal body -->
+                                            <div class="p-4 md:p-5 space-y-4">
+                                                <p>
+                                                    Заказ  #{{ $order->id }}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
         </div>
-
     </div>
 @endsection
